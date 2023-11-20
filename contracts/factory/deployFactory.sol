875e878f4a71802521fca9c955869230b7329dd7
midesofek <midesofek@gmail.com>
2023-11-20 08:16:34 +0100

    added deploy factory contract

---
+++ contracts/factory/deployFactory.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.0;
+
+contract DeployFactory {
+    event Deploy(address tokenContractAddress);
+
+    function deploy(bytes memory bytecode, uint _salt) external {
+        address tokenContractAddress;
+        assembly {
+            tokenContractAddress := create2(
+                0,
+                add(bytecode, 0x20),
+                mload(bytecode),
+                _salt
+            )
+            if iszero(extcodesize(tokenContractAddress)) {
+                revert(0, 0)
+            }
+        }
+        emit Deploy(tokenContractAddress);
+    }
+}
