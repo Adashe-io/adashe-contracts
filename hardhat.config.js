25aceb5deb0588a2092763a0fc9b3d627122a7c9
midesofek <midesofek@gmail.com>
2023-11-20 07:52:28 +0100

    setting up for deterministic contract deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,62 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const { task } = require("hardhat/config");
+const { API_URL_GOERLI, API_URL_MUMBAI, API_URL_SEPOLIA, PRIVATE_KEY } =
+  process.env;
 
-/** @type import('hardhat/config').HardhatUserConfig */
+task(
+  "account",
+  "returns nonce and balance for specified address on multiple networks"
+)
+  .addParam("address")
+  .setAction(async (address) => {
+    const web3Goerli = createAlchemyWeb3(API_URL_GOERLI);
+    const web3Sepolia = createAlchemyWeb3(API_URL_SEPOLIA);
+    const web3Mumbai = createAlchemyWeb3(API_URL_MUMBAI);
+
+    const networkIDArr = [
+      "Ethereum Goerli:",
+      "Ethereum Sepolia:",
+      "Polygon  Mumbai:",
+    ];
+    const providerArr = [web3Goerli, web3Mumbai, web3Sepolia];
+    const resultArr = [];
+
+    for (let i = 0; i < providerArr.length; i++) {
+      const nonce = await providerArr[i].eth.getTransactionCount(
+        address.address,
+        "latest"
+      );
+      const balance = await providerArr[i].eth.getBalance(address.address);
+      resultArr.push([
+        networkIDArr[i],
+        nonce,
+        parseFloat(providerArr[i].utils.fromWei(balance, "ether")).toFixed(2) +
+          "ETH",
+      ]);
+    }
+    resultArr.unshift(["  |NETWORK|   |NONCE|   |BALANCE|  "]);
+    console.log(resultArr);
+  });
+
+// /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  solidity: "0.8.20",
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: API_URL_GOERLI,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    sepolia: {
+      url: API_URL_SEPOLIA,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+    mumbai: {
+      url: API_URL_MUMBAI,
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
