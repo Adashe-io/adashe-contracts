02b5b628dd307f68bee46ee111be8b668ea0bccd
midesofek <midesofek@gmail.com>
2023-11-22 12:18:16 +0100

    configured hardhat.config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,44 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const { task } = require("hardhat/config");
-const { API_URL_GOERLI, API_URL_MUMBAI, API_URL_SEPOLIA, PRIVATE_KEY } =
-  process.env;
+const checkNonce = require("./utils/checkNonce");
+const {
+  API_URL_GOERLI,
+  API_URL_MUMBAI,
+  API_URL_SEPOLIA,
+  API_URL_ETHMAINNET_KEY,
+  API_URL_POLYGON_KEY,
+  PRIVATE_KEY,
+  MAINNET_PRIVATE_KEY,
+} = process.env;
 
 task(
   "account",
   "returns nonce and balance for specified address on multiple networks"
 )
   .addParam("address")
-  .setAction(async (address) => {
-    const web3Goerli = createAlchemyWeb3(API_URL_GOERLI);
-    const web3Sepolia = createAlchemyWeb3(API_URL_SEPOLIA);
-    const web3Mumbai = createAlchemyWeb3(API_URL_MUMBAI);
-
-    const networkIDArr = [
-      "Ethereum Goerli:",
-      "Ethereum Sepolia:",
-      "Polygon  Mumbai:",
-    ];
-    const providerArr = [web3Goerli, web3Mumbai, web3Sepolia];
-    const resultArr = [];
-
-    for (let i = 0; i < providerArr.length; i++) {
-      const nonce = await providerArr[i].eth.getTransactionCount(
-        address.address,
-        "latest"
-      );
-      const balance = await providerArr[i].eth.getBalance(address.address);
-      resultArr.push([
-        networkIDArr[i],
-        nonce,
-        parseFloat(providerArr[i].utils.fromWei(balance, "ether")).toFixed(2) +
-          "ETH",
-      ]);
-    }
-    resultArr.unshift(["  |NETWORK|   |NONCE|   |BALANCE|  "]);
-    console.log(resultArr);
-  });
+  .setAction(async (address) => checkNonce(address));
 
 // /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -46,6 +25,14 @@ module.exports = {
   solidity: "0.8.20",
   networks: {
     hardhat: {},
+    ethmainnet: {
+      url: API_URL_ETHMAINNET_KEY,
+      accounts: [`0x${MAINNET_PRIVATE_KEY}`],
+    },
+    polygon: {
+      url: API_URL_POLYGON_KEY,
+      accounts: [`0x${MAINNET_PRIVATE_KEY}`],
+    },
     goerli: {
       url: API_URL_GOERLI,
       accounts: [`0x${PRIVATE_KEY}`],
